a7182fc7802f981b0bd26138c9b377879d45e9fd
Zansuken <doom.sebastien.pro@gmail.com>
2021-11-29 20:09:43 +0100

    feat(confirmation): added confirmation view

--- client/javascript/confirmation.js
+++ client/javascript/confirmation.js
@@ -4,20 +4,30 @@ fetch("http://localhost:3000/api/cameras")
     
     console.log(value);
     const layout = document.querySelector("article");
-    
-    for (const card of value) {
+
+    const orderRef = Math.random().toString(36).substr(2, 10);
 
         const newDiv = document.createElement('div')
-        let itemPrice = convertPrice(`${card.price}`);
-        let itemProductPage = card._id
+        let itemPrice = convertPrice(`${value[0].price}`);
+        let taxesFee = convertPrice(value[0].price * 0.2)
+        let itemProductPage = value[0]._id
         newDiv.innerHTML = `
-        <div class="card">
-            <ul>
-            </ul>
-        </div>
+            <div id="payment_view">
+                <h2>Order Summary</h2>
+                <span>Order reference: <strong>${orderRef}</strong></span>
+                <span>Shipping: <strong>free</strong></span>
+                <span>Taxes: <strong>${taxesFee}</strong></span>
+                <span>Total: <strong>${itemPrice}</strong></span>
+                <div id="payments_methods">
+                    <img src="images/visa-icon.svg" alt="visa icon">
+                    <img src="images/master-card-icon.svg" alt="master card icon">
+                    <img src="images/paypal-icon.svg" alt="paypal icon">
+                </div>
+                <button id="pay_button">Continue to secure payment</button>
+                <a href="cart.html">Cancel payment</a>
+            </div>
         `
         layout.append(newDiv)
-    }
 
 })
 .catch(function(err) {
