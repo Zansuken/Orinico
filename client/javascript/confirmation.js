06fc7bb88e522483f7144694e92fb94afe8c5e21
Zansuken <doom.sebastien.pro@gmail.com>
2021-12-01 17:58:11 +0100

    feat(confirmation): added a checked state to payment method buttons

--- client/javascript/confirmation.js
+++ client/javascript/confirmation.js
@@ -3,29 +3,55 @@ fetch("http://localhost:3000/api/cameras")
 .then(async function(value) {
     
     const layout = document.querySelector("article");
+    const newDiv = document.createElement('div')
+    let total = totalAmount
 
-    const orderRef = Math.random().toString(36).substr(2, 10);
+    if (totalAmount > 0) {
+        const orderRef = Math.random().toString(36).substr(2, 10);
 
-        const newDiv = document.createElement('div')
-        let total = localStorage.getItem('totalToPay')
         let taxesFee = convertPrice(total * 0.2)
         newDiv.innerHTML = `
-            <div id="payment_view">
-                <h2>Order Summary</h2>
-                <span>Order reference: <strong>${orderRef}</strong></span>
-                <span>Shipping: <strong>free</strong></span>
-                <span>Taxes: <strong>${taxesFee}</strong></span>
-                <span>Total: <strong>${convertPrice(total)}</strong></span>
-                <div id="payments_methods">
-                    <img src="images/visa-icon.svg" alt="visa icon">
-                    <img src="images/master-card-icon.svg" alt="master card icon">
-                    <img src="images/paypal-icon.svg" alt="paypal icon">
-                </div>
-                <button id="pay_button">Continue to secure payment</button>
-                <a href="cart.html">Cancel payment</a>
+        <div id="payment_view">
+            <h2>Order Summary</h2>
+            <span>Order reference: <strong>${orderRef}</strong></span>
+            <span>Shipping: <strong>free</strong></span>
+            <span>Taxes: <strong>${taxesFee}</strong></span>
+            <span>Total: <strong>${convertPrice(total)}</strong></span>
+            <div id="payments_methods">
+                <img class="payment_options" src="images/visa-icon.svg" alt="visa icon">
+                <img class="payment_options" src="images/master-card-icon.svg" alt="master card icon">
+                <img class="payment_options" src="images/paypal-icon.svg" alt="paypal icon">
             </div>
-        `
-        layout.append(newDiv)
+            <button id="pay_button">Continue to secure payment</button>
+            <a href="cart.html">Cancel payment</a>
+        </div>
+    `
+    layout.append(newDiv)
+    const paymentMethodBtn = document.querySelectorAll('.payment_options')
+    for (const btn of paymentMethodBtn) {
+        let backGroundWhite = false
+        btn.addEventListener('click', () => {
+            if (backGroundWhite === false) {
+                btn.style.background = "rgb(56, 19, 156)"
+                backGroundWhite = true
+            } else {
+                btn.style.background = "white"
+                backGroundWhite = false
+            }
+        })
+    }
+    } else {
+        newDiv.innerHTML = `
+        <div id="empty_cart">
+            <h2>There was nothing in your cart :(...</h2>
+            <a href="index.html">Go back to shopping!</a>
+        </div>
+    `
+    layout.append(newDiv)
+    }
+
+
+    
 
 })
 .catch(function(err) {
