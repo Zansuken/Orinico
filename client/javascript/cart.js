63ae9341e4f908356f3a801a91e0733e400b6397
Zansuken <doom.sebastien.pro@gmail.com>
2021-12-03 19:26:58 +0100

    feat(cart): changed preview design and added an alternate view if cart is empty

--- client/javascript/cart.js
+++ client/javascript/cart.js
@@ -6,44 +6,64 @@ fetch("http://localhost:3000/api/cameras")
 
     const cartList = document.querySelector("#cart_ul");
 
-    // Loop inside localStorage "basket" object and create a new line in the cart for each items
+    if (basket.length == 0) {
+        const newDiv = document.createElement('div')
+        const cartView = document.querySelector('.cart_list')
+        const cartPreview = document.querySelector('#cart_preview')
 
-    for (const item of basket) {
-        let newLi = document.createElement("li")
-        newLi.innerHTML = `<img src="${item.imageUrl}" alt="camera" class="item_image">
-        <div class="item_details">
-            <h2>${item.name}</h2>
-            <p>${item.description}</p>
-            <span id="option_selected"><strong>Options: ${item.option}</strong></span>
-            <span id="item_quantity"><strong>Quantity: ${item.quantity}</strong> </span>
+        cartList.remove()
+        cartPreview.style.display = "none"
+        
+
+        newDiv.innerHTML = `
+        <div id="empty_cart">
+            <h2>There was nothing in your cart :(...</h2>
+            <a href="index.html">Go back to shopping!</a>
         </div>
-        <div class="item_side">
-            <div class="item_buttons">
-            <button id="delete_button"><img src="images/delete.svg" alt="rubish container icon"></button>
-            </div>
-            <span id="selected_item_total">${convertPrice(item.price * item.quantity)}</span>
-        </div>`
-        cartList.appendChild(newLi)
-    }
+        `
+        cartView.append(newDiv)
+    } else {
 
-    // Loop inside localStorage "basket" object then set total in charge using quantities and prices of items
-    // Send updated total to localStorage "totalToPay"
-
-    for (const item of basket) {
-        totalToPay = totalToPay += (item.price * item.quantity);
-        localStorage.setItem("total", JSON.stringify(totalToPay));
-    }
-
-    // Update total in charge view
-
-    document.querySelector('#cart_preview span').innerHTML = "Total: " + convertPrice(totalToPay);
-
-    // Clear all localStorage items then reload the page to refresh view
+        // Loop inside localStorage "basket" object and create a new line in the cart for each items
     
-    document.querySelector('#cart_preview #delete_button').addEventListener('click', () => {
-        clearBasket()
-        window.location.reload()
-    })
+        for (const item of basket) {
+            let newLi = document.createElement("li")
+            newLi.innerHTML = `<img src="${item.imageUrl}" alt="camera" class="item_image">
+            <div class="item_details">
+                <h2>${item.name}</h2>
+                <p>${item.description}</p>
+                <span id="option_selected"><strong>Options: ${item.option}</strong></span>
+                <span id="item_quantity"><strong>Quantity: ${item.quantity}</strong> </span>
+            </div>
+            <div class="item_side">
+                <div class="item_buttons">
+                <button id="delete_button"><img src="images/delete.svg" alt="rubish container icon"></button>
+                </div>
+                <span id="selected_item_total">${convertPrice(item.price * item.quantity)}</span>
+            </div>`
+            cartList.appendChild(newLi)
+        }
+    
+        // Loop inside localStorage "basket" object then set total in charge using quantities and prices of items
+        // Send updated total to localStorage "totalToPay"
+    
+        for (const item of basket) {
+            totalToPay = totalToPay += (item.price * item.quantity);
+            localStorage.setItem("total", JSON.stringify(totalToPay));
+        }
+    
+        // Update total in charge view
+    
+        document.querySelector('#cart_preview span').innerHTML = "Total: " + convertPrice(totalToPay);
+    
+        // Clear all localStorage items then reload the page to refresh view
+        
+        document.querySelector('#cart_preview #delete_button').addEventListener('click', () => {
+            clearBasket()
+            window.location.reload()
+        })
+    }
+
 })
 .catch(function(err) {
     return err
